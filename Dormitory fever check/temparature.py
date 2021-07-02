9aa0b506e5758ce07b92a86b4dfa6a9058e600fe
Kei-Seo <ghdtjr3824@naver.com>
2021-07-02 16:39:49 +0900

    업데이트

--- Dormitory fever check/temparature.py
+++ Dormitory fever check/temparature.py
@@ -2,47 +2,46 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('Cchromedriver_win32chromedriver.exe')
-url = 'httpsdorm.cnu.ac.krintranetlogin.php'
+driver = webdriver.Chrome('C:/chromedriver_win32/chromedriver.exe')
+url = 'https://dorm.cnu.ac.kr/intranet/login.php'
 driver.get(url)
-xpath = input[@name='idno']
+xpath = "//input[@name='idno']"
 id_btn = driver.find_element_by_xpath(xpath)
 id_btn.send_keys(201702026)
 
-xpath = input[@name='pass']
+xpath = "//input[@name='pass']"
 pass_btn = driver.find_element_by_xpath(xpath)
-pass_btn.send_keys(cheld!#5)
+pass_btn.send_keys("cheld!#5")
 
-xpath = input[@class='login_btn']
+xpath = "//input[@class='login_btn']"
 enter_btn = driver.find_element_by_xpath(xpath)
 enter_btn.click()
 
 time.sleep(2)
-driver.get('httpsdorm.cnu.ac.krintranetusercorona19_daychkform.php')
+driver.get('https://dorm.cnu.ac.kr/intranet/user/corona19_daychkform.php')
 time.sleep(2)
 
-xpath = input[contains(@name,'q3') and contains(@value,'N')]
+xpath = "//input[contains(@name,'agreeall')]"
 q3_btn = driver.find_element_by_xpath(xpath)
 q3_btn.click()
 
-xpath = input[contains(@name,'q2') and contains(@value,'N')]
-q2_btn = driver.find_element_by_xpath(xpath)
-q2_btn.click()
-
-xpath = input[contains(@name,'agree') and contains(@value,'Y')]
-agree_btn = driver.find_element_by_xpath(xpath)
-agree_btn.click()
-
-xpath = input[@name='agree2']
-agree2_btn = driver.find_element_by_xpath(xpath)
-agree2_btn.click()
-
-xpath = input[@class='btn_blue']
+# xpath = "//input[contains(@name,'q2') and contains(@value,'N')]"
+# q2_btn = driver.find_element_by_xpath(xpath)
+# q2_btn.click()
+#
+# xpath = "//input[contains(@name,'agree') and contains(@value,'Y')]"
+# agree_btn = driver.find_element_by_xpath(xpath)
+# agree_btn.click()
+#
+# xpath = "//input[@name='agree2']"
+# agree2_btn = driver.find_element_by_xpath(xpath)
+# agree2_btn.click()
+#
+xpath = "//input[@class='btn_blue']"
 complate = driver.find_element_by_xpath(xpath)
 complate.click()
 
 alert = driver.switch_to.alert
 alert.accept()
 alert.accept()
-
 
---
+++ Dormitory fever check/temparature.spec
@@ -0,0 +1,38 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+block_cipher = None
+
+
+a = Analysis(['temparature.py'],
+             pathex=['C:\\Users\\ghdtj\\OneDrive\\바탕 화면\\web crawling\\Dormitory fever check'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          [],
+          exclude_binaries=True,
+          name='temparature',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               upx_exclude=[],
+               name='temparature')
